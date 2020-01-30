bee41c524bcd7e4e89dd1d7560b97ce5db6e7b0e
GraemeKing14 <60473278+GraemeKing14@users.noreply.github.com>
2020-01-30 14:33:49 +0000

    Update teamName.py

--- .github/workflows/teamName.py
+++ .github/workflows/teamName.py
@@ -2,9 +2,19 @@ class hellothere:
   def __init__(self):
       self.name = "" 
       self.teamName = ""
+      self.member = []
       
   def getTeamName(self):
       return self.teamName
      
   def setTeamName(self, n):
       self.teamName = n
+  
+  def getMemberName():
+      return self.name
+    
+  def setMemberName(self, n):
+      self.name = n
+      
+  def addMember(self, m):
+      self.member.append(m)
