a831a02ed2ce6132bfbe488ba2bef16b021eca5e
GraemeKing14 <60473278+GraemeKing14@users.noreply.github.com>
2020-01-30 14:24:37 +0000

    Create teamName.py

---
+++ .github/workflows/teamName.py
@@ -0,0 +1,10 @@
+class hellothere:
+  def __init__(self):
+      self.name = "" 
+      self.teamName = ""
+      
+  def getTeamName(self):
+      return self.teamName
+     
+  def setTeamName(self, n):
+      self.teamName = n
